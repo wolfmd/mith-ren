cfa88fa494ac4c162c3e6673bc2124977131425a
lain <lain@cyberia.cyberia>
2016-11-09 07:29:05 -0800

    Created basic daemon that sniffs and prints to console. Now must parse and act upon that data.

--- mith-rend.py
+++ mith-rend.py
@@ -4,20 +4,55 @@
 #
 
 import time
-from daemon import runner
+import logging
+import subprocess
+import yaml
+import os
+from screenutils import list_screens, Screen
+import mousejack
 
 class App():
     def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
-        self.pidfile_path =  '/etc/mith-ren/logs/pid'
-        self.pidfile_timeout = 5
+        hello = "yes"
     def run(self):
-        while True:
-            print("I'm hacking the gibson!")
-            time.sleep(10)
+        logging.basicConfig(filename='example.log',level=logging.DEBUG)
+        with open("mith-rend.conf", 'r') as stream:
+            try:
+                config = yaml.load(stream)
+                logging.debug("Successfully loaded configuration file")
+            except yaml.YAMLError as exc:
+                logging.warning("Could not read YAML File:%s" % exc)
+
+#Going to add decoupling eventually
+
+        for module in config['modules']:
+            if 'mousejack' in module:
+                logging.debug("Loaded mousejack module")
+                # bottlewhack = mousejack.Mousejack()
+                # bottlewhack.run()
+                with open('output.txt', 'w') as f:
+                    subprocess.Popen(["sudo python modules/mousejack/tools/nrf24-scanner.py  --verbose"], stdout = f, stderr = f, shell=True)
+                with open('output.txt', 'r') as o:
+                    o.seek(0,2) # Go to the end of the file
+                    while True:
+                        line = o.readline()
+                        if not line:
+                            time.sleep(0.1) # Sleep briefly
+                            continue
+                        print line
+
+                # wait for the process to terminate
+                # out, err = process.communicate()
+                # errcode = process.returncode
+                # print out
+
+
 
 app = App()
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
+app.run()
+
+#TODO
+# Make sure to truncate the output file
+# Make sure to kill subprocesses...
+#
+#
---
+++ mousejack.py
@@ -0,0 +1,14 @@
+import time
+import logging
+import subprocess
+import yaml
+from screenutils import list_screens, Screen
+import mousejack
+
+class Mousejack():
+    def __init__(self):
+        hello = "yes"
+    def run(self):
+        with open('output.txt', 'w') as f:
+            subprocess.Popen(["sudo python modules/mousejack/tools/nrf24-scanner.py  --verbose"], stdout = f, stderr = f, shell=True)
+        print "yes"
