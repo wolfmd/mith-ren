402766446cf902cb2fa92f4868f69559bc50e04f
wolfmd <wolfmd@mail.uc.edu>
2017-02-05 16:48:32 -0800

    Typo

--- app/mithren-frontend.py
+++ app/mithren-frontend.py
@@ -123,7 +123,7 @@ class MithrendFrontend():
 
         # View Log Forever
         elif command == "5":
-            while true:
+            while True:
                 print("\nWhat would you like to follow?:\n[1] Daemon Log [2] Mousejack Scanner [3] Found Devices [4] Found Packets [5] Follow Devices [6] Go back")
                 sub_command = self.get_input()
                 if sub_command == "1":
